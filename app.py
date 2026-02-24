fe8abbe2c3768caf34a0d6894ad84eb6bbe6a7f0
xworms <peihcra@gmail.com>
2026-02-24 13:10:36 +0000

    hello

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, joinedload
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
